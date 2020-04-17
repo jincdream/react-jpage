38b9be16e1cfa9a1c3f75d3cb785e45ea7ae87f5
jinc.cjc <jinc.cjc@alibaba-inc.com>
2020-04-17 14:48:27 +0800

    fix: columns

--- src/layout.tsx
+++ src/layout.tsx
@@ -12,7 +12,7 @@ export interface ILayoutProps {
 export default class Layout extends Component<ILayoutProps, ILayoutProps> {
   state = this.props
   componentWillReceiveProps(props: ILayoutProps) {
-    let keys = ["colunms", "rows", "template"] as (keyof ILayoutProps)[]
+    let keys = ["columns", "rows", "template"] as (keyof ILayoutProps)[]
     let state = updateSate<ILayoutProps>(keys, props, this.state)
     this.setState(state)
   }
